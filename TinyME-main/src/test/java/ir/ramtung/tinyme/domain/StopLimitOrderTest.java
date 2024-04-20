34446b2bf3622f71d5af17db7e97732e2064c4d7
Motaee <mamad.motaee@gmail.com>
2024-04-20 18:35:11 +0330

    tests

--- TinyME-main/src/test/java/ir/ramtung/tinyme/domain/StopLimitOrderTest.java
+++ TinyME-main/src/test/java/ir/ramtung/tinyme/domain/StopLimitOrderTest.java
@@ -112,11 +112,63 @@ public class StopLimitOrderTest {
         Trade trade1 = new Trade(security, matchingBuyOrder.getPrice(), matchingSellOrder1.getQuantity(),
                 matchingBuyOrder, matchingSellOrder1);
 
-        //assertThat(sell_broker.getCredit()).isEqualTo(100_000_000L);
         verify(eventPublisher).publish((new OrderAcceptedEvent(1, 200)));
         verify(eventPublisher).publish(new OrderExecutedEvent(1, 200, List.of(new TradeDTO(trade1))));
         verify(eventPublisher).publish((new OrderActivatedEvent(1, 11)));
         verify(eventPublisher).publish(new OrderExecutedEvent(1, 11, List.of(new TradeDTO(trade2))));
+    }
+
+    @Test
+    void new_order_triggers_stop_limit_order() {
+        StopLimitOrder matchingStopLimitOrder = new StopLimitOrder(11, security, BUY, 200, 15800,
+                buy_broker, shareholder, 15700);
+        Order matchingSellOrder = new Order(6, security, Side.SELL, 350, 15800, sell_broker,
+                shareholder, 0);
+        StopLimitOrder matchingSellOrder1 = new StopLimitOrder(200, security, SELL, 300, 15600,
+                sell_broker, shareholder, 0);
+        Order matchingBuyOrder= new Order(1, security, BUY, 304, 15700, buy_broker, shareholder,
+                0);
+
+
+        orderHandler.handleEnterOrder(EnterOrderRq.createNewOrderRq(1, security.getIsin(),
+                200, LocalDateTime.now(), Side.SELL, 300, 15600,
+                sell_broker.getBrokerId(), shareholder.getShareholderId(), 0,
+                0, 0));
+
+        verify(eventPublisher).publish((new OrderAcceptedEvent(1, 200)));
+        verify(eventPublisher).publish((new OrderActivatedEvent(1, 11)));
+    }
+
+    @Test
+    void new_order_triggers_stop_limit_order_and_trades_and_check_credit_after_trade() {
+        StopLimitOrder matchingStopLimitOrder = new StopLimitOrder(11, security, BUY, 200, 15800,
+                buy_broker, shareholder, 15700);
+        Order matchingSellOrder = new Order(6, security, Side.SELL, 350, 15800, sell_broker,
+                shareholder, 0);
+        StopLimitOrder matchingSellOrder1 = new StopLimitOrder(200, security, SELL, 300, 15600,
+                sell_broker, shareholder, 0);
+        Order matchingBuyOrder= new Order(1, security, BUY, 304, 15700, buy_broker, shareholder,
+                0);
+
+
+        orderHandler.handleEnterOrder(EnterOrderRq.createNewOrderRq(1, security.getIsin(),
+                200, LocalDateTime.now(), Side.SELL, 300, 15600,
+                sell_broker.getBrokerId(), shareholder.getShareholderId(), 0,
+                0, 0));
+
+        Trade trade2 = new Trade(security, matchingStopLimitOrder.getPrice(), matchingStopLimitOrder.getQuantity(),
+                matchingStopLimitOrder, matchingSellOrder);
+
+        Trade trade1 = new Trade(security, matchingBuyOrder.getPrice(), matchingSellOrder1.getQuantity(),
+                matchingBuyOrder, matchingSellOrder1);
+
+        verify(eventPublisher).publish((new OrderAcceptedEvent(1, 200)));
+        verify(eventPublisher).publish((new OrderActivatedEvent(1, 11)));
+
+        int sell_credit = 100000000 +  (matchingBuyOrder.getPrice() * matchingSellOrder1.getQuantity()) + (matchingStopLimitOrder.getPrice() * matchingStopLimitOrder.getQuantity()) ;
+        assertThat(sell_broker.getCredit()).isEqualTo(sell_credit);
+
+
     }
 
 }
