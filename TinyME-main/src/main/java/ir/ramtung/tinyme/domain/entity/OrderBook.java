f56255404529af0302f905a09c7e24e3aba3cee4
Arash444 <arash.shahin.444@gmail.com>
2024-04-22 17:24:10 +0330

    Revert "Revert matchWithFirst" for now
    This reverts commit 6c196888f414852b816426932ddf27fef780dfe3.

--- TinyME-main/src/main/java/ir/ramtung/tinyme/domain/entity/OrderBook.java
+++ TinyME-main/src/main/java/ir/ramtung/tinyme/domain/entity/OrderBook.java
@@ -55,10 +55,12 @@ public class OrderBook {
 
     public Order matchWithFirst(Order newOrder) {
         var queue = getQueue(newOrder.getSide().opposite());
-        if (newOrder.matches(queue.getFirst()))
-            return queue.getFirst();
-        else
-            return null;
+        for (Order matchedOrder : queue) {
+            if (newOrder.matches(matchedOrder) && matchedOrder.canTrade()) {
+                return matchedOrder;
+            }
+        }
+        return null;
     }
 
     public void putBack(Order order) {
